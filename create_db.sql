b0f307d9d2e9bc5d7fbac278e73e37488ee16d51
Norman Sue <normful@gmail.com>
2014-06-01 15:14:43 -0700

    replace tab with 4 spaces

--- create_db.sql
+++ create_db.sql
@@ -96,6 +96,6 @@ CREATE TABLE `Referees` (
     `awayTeam` CHAR(3),
     PRIMARY KEY (`refNumber`, `gameDate`, `homeTeam`, `awayTeam`),
     FOREIGN KEY (`refNumber`) REFERENCES `NBAReferee` (`number`),
-    FOREIGN KEY (`gameDate`, `homeTeam`, `awayTeam`) 
-		REFERENCES `NBAGame_Plays_PlayedAt` (`gameDate`, `homeTeam`, `awayTeam`)
+    FOREIGN KEY (`gameDate`, `homeTeam`, `awayTeam`)
+        REFERENCES `NBAGame_Plays_PlayedAt` (`gameDate`, `homeTeam`, `awayTeam`)
 );
