a40606daf9e4c6f5d34b718220887deb5e44e278
Norman Sue <normful@gmail.com>
2014-06-13 04:20:20 -0700

    make filepaths relative in insert_data.sql script

--- insert_data.sql
+++ insert_data.sql
@@ -1,27 +1,27 @@
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/Division.txt' 
-    INTO TABLE nba.division
+LOAD DATA INFILE './data/Division.txt' 
+    INTO TABLE nbatest1.division
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/NBAGame_Plays_PlayedAt.txt'
-    INTO TABLE nba.nbagame_plays_playedat
+LOAD DATA INFILE './data/NBAGame_Plays_PlayedAt.txt'
+    INTO TABLE nbatest1.nbagame_plays_playedat
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/NBAPlayer_PlaysFor.txt'
-    INTO TABLE nba.nbaplayer_playsfor
+LOAD DATA INFILE './data/NBAPlayer_PlaysFor.txt'
+    INTO TABLE nbatest1.nbaplayer_playsfor
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/NBAReferee.txt'
-    INTO TABLE nba.nbareferee
+LOAD DATA INFILE './data/NBAReferee.txt'
+    INTO TABLE nbatest1.nbareferee
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/NBAStaff_WorksFor.txt'
-    INTO TABLE nba.nbastaff_worksfor
+LOAD DATA INFILE './data/NBAStaff_WorksFor.txt'
+    INTO TABLE nbatest1.nbastaff_worksfor
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/NBATeam_BelongsTo.txt'
-    INTO TABLE nba.nbateam_belongsto
+LOAD DATA INFILE './data/NBATeam_BelongsTo.txt'
+    INTO TABLE nbatest1.nbateam_belongsto
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/Referees.txt'
-    INTO TABLE nba.referees
+LOAD DATA INFILE './data/Referees.txt'
+    INTO TABLE nbatest1.referees
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/Sponsor_Endorses.txt'
-    INTO TABLE nba.sponsor_endorses
+LOAD DATA INFILE './data/Sponsor_Endorses.txt'
+    INTO TABLE nbatest1.sponsor_endorses
     IGNORE 1 LINES;
-LOAD DATA INFILE '/opt/bitnami/apache2/cpsc304/data/Venue.txt'
-    INTO TABLE nba.venue
+LOAD DATA INFILE './data/Venue.txt'
+    INTO TABLE nbatest1.venue
     IGNORE 1 LINES;
