f970bcc6267c870b4ea27a261655599ae56fa0d9
Daniel Tsang <Danyull@dhcp-128-189-196-95.ubcsecure.wireless.ubc.ca>
2014-06-13 11:13:04 -0700

    Revert 40d1529..c12c032
    This rolls back to commit 40d1529c924e462a136ab4041779d158a58f0076.

--- src/games.php
+++ src/games.php
@@ -99,9 +99,6 @@ $result->setFetchMode(PDO::FETCH_ASSOC);
 						<td><?php echo $row['city']?></td>
 						<td><?php echo $row['refNumber']; ?></td>
 						<td>
-							<!-- <a href="delete_player.php?number=<?php echo $row['number']; ?>&team=<?php echo $row['team']; ?>">
-								<span class="glyphicon glyphicon-remove"></span>
-							</a> -->
 						</td>
 					</tr>
 				<?php endwhile; ?>
@@ -143,7 +140,7 @@ $result->setFetchMode(PDO::FETCH_ASSOC);
 				$result->bindParam('.a', $gDate1);
 				$result->bindParam('.b', $gDate2);
 				$result->execute();
-				while ($row = $result->fetch()) {
+				for ($i=0; $row = $result->fetch(); $i++) {
 					?>
 					<tr>
 						<td><?php echo $row['gameDate']?></td>
@@ -155,7 +152,7 @@ $result->setFetchMode(PDO::FETCH_ASSOC);
 						<td><?php echo $row['city']?></td>
 						<td><?php echo $row['refNumber']; ?></td>
 					</tr>
-					<?php endwhile ?>
+					<?php } ?>
 			</tbody>
 		</table>
 	</div>
