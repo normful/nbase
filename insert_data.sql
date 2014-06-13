5f7bfc535aafb3852708274cf7ea315307de34d7
Norman Sue <normful@gmail.com>
2014-06-13 04:21:33 -0700

    fix table name in insert script

--- insert_data.sql
+++ insert_data.sql
@@ -1,27 +1,27 @@
 LOAD DATA INFILE './data/Division.txt' 
-    INTO TABLE nbatest1.division
+    INTO TABLE nba.division
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/NBAGame_Plays_PlayedAt.txt'
-    INTO TABLE nbatest1.nbagame_plays_playedat
+    INTO TABLE nba.nbagame_plays_playedat
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/NBAPlayer_PlaysFor.txt'
-    INTO TABLE nbatest1.nbaplayer_playsfor
+    INTO TABLE nba.nbaplayer_playsfor
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/NBAReferee.txt'
-    INTO TABLE nbatest1.nbareferee
+    INTO TABLE nba.nbareferee
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/NBAStaff_WorksFor.txt'
-    INTO TABLE nbatest1.nbastaff_worksfor
+    INTO TABLE nba.nbastaff_worksfor
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/NBATeam_BelongsTo.txt'
-    INTO TABLE nbatest1.nbateam_belongsto
+    INTO TABLE nba.nbateam_belongsto
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/Referees.txt'
-    INTO TABLE nbatest1.referees
+    INTO TABLE nba.referees
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/Sponsor_Endorses.txt'
-    INTO TABLE nbatest1.sponsor_endorses
+    INTO TABLE nba.sponsor_endorses
     IGNORE 1 LINES;
 LOAD DATA INFILE './data/Venue.txt'
-    INTO TABLE nbatest1.venue
+    INTO TABLE nba.venue
     IGNORE 1 LINES;
