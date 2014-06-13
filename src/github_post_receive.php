8f0dba59eaf35afe805c8ad5a7d5462c1d2116d3
Norman Sue <normful@gmail.com>
2014-06-13 02:16:28 -0700

    use different github post receive script

--- src/github_post_receive.php
+++ src/github_post_receive.php
@@ -1,7 +1,8 @@
 <?php
-
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /home/project/cpsc304 && git pull');
-}
-
-?>Github post receive page
+$gitpath = '/opt/bitnami/git/bin/git';
+header("Content-type: text/plain"); // be explicit to avoid accidental XSS
+// example: git root is one level above the directory that contains this file
+chdir(__DIR__ . '/../'); // rarely actually an acceptable thing to do
+system("/usr/bin/env -i {$gitpath} pull 2>&1"); // main repo (current branch)
+echo "\nDone.\n";
+?>
