d842aa2c556d3b8eb661a325a74e800adb1094e3
Norman Sue <normful@gmail.com>
2014-06-14 23:24:58 -0700

    revise update player form

--- src/forms/update_player.php
+++ src/forms/update_player.php
@@ -10,39 +10,41 @@ $allTeamsResult->setFetchMode(PDO::FETCH_ASSOC);
 
 ?>
 
-<form method="POST" class="form-horizontal">
-    <div class="form-group">
-        <label for="newFirstName" class="control-label col-xs-2">First Name</label>
-        <div class="col-xs-10">
-            <input type="text" class="form-control" id="newFirstName" placeholder="oldFirstName">
+<div class="container">
+    <form method="POST" class="form-horizontal">
+
+        <div class="form-group">
+            <label for="newFirstName" class="control-label col-xs-2">First Name</label>
+            <div class="col-xs-5">
+                <input type="text" class="form-control" name="newFirstName" placeholder="oldFirstName">
+            </div>
         </div>
-    </div>
 
-    <div class="form-group">
-        <label for="newPosition" class="control-label col-xs-2">Position</label>
-        <select name="newPosition">
-            <option value="PG">PG</option>
-            <option value="SG">SG</option>
-            <option value="SF">SF</option>
-            <option value="PF">PF</option>
-            <option value="C">C</option>
-        </select>
-    </div>
-
-<!--
-    <select name="newTeamName">
-        <?php while ($row = $allTeamsResult ->fetch()): ?>
-        <option value="<?php echo $row['abbreviation']; ?>">
-            <?php echo $row['teamName'] . " (" . $row['abbreviation'] . ")"; ?>
-        </option>
-        <?php endwhile; ?>
-    <select>
--->
-
-    <div class="form-group">
-        <div class="col-xs-offset-2 col-xs-10">
-            <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group">
+            <label for="newLastName" class="control-label col-xs-2">Last Name</label>
+            <div class="col-xs-5">
+                <input type="text" class="form-control" name="newLastName" placeholder="oldLastName">
+            </div>
         </div>
-    </div>
 
-</form>
+        <div class="form-group">
+            <label for="newPosition" class="control-label col-xs-2">Position</label>
+            <div class="col-xs-5">
+                <select name="newPosition" class="form-control">
+                    <option value="PG">PG</option>
+                    <option value="SG">SG</option>
+                    <option value="SF">SF</option>
+                    <option value="PF">PF</option>
+                    <option value="C">C</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-xs-offset-2 col-xs-10">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+</div>
