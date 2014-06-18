799c07e1b3f5a0051cfeac1828d9998f2e8d0c87
Norman Sue <normful@gmail.com>
2014-06-17 22:03:14 -0700

    fix bug in update.php so that placeholders are values

--- src/forms/update_player.php
+++ src/forms/update_player.php
@@ -4,14 +4,14 @@
         <div class="form-group">
             <label class="control-label col-xs-2">First Name</label>
             <div class="col-xs-5">
-                <input type="text" class="form-control" name="newFirstName" placeholder="<?php echo $oldFirstName;?>" pattern="[A-Za-z]+">
+                <input type="text" class="form-control" name="newFirstName" value="<?php echo $oldFirstName;?>" pattern="[A-Za-z]+">
             </div>
         </div>
 
         <div class="form-group">
             <label class="control-label col-xs-2">Last Name</label>
             <div class="col-xs-5">
-                <input type="text" class="form-control" name="newLastName" placeholder="<?php echo $oldLastName;?>" pattern="[A-Za-z]+">
+                <input type="text" class="form-control" name="newLastName" value="<?php echo $oldLastName;?>" pattern="[A-Za-z]+">
             </div>
         </div>
 
@@ -36,7 +36,7 @@
         <div class="form-group">
             <label class="control-label col-xs-2">Number</label>
             <div class="col-xs-5">
-                <input type="number" class="form-control" name="newNumber" placeholder="<?php echo $oldNumber;?>" min="0" max="99">
+                <input type="number" class="form-control" name="newNumber" value="<?php echo $oldNumber;?>" min="0" max="99">
             </div>
         </div>
 
@@ -63,21 +63,21 @@
         <div class="form-group">
             <label class="control-label col-xs-2">Weight (lbs)</label>
             <div class="col-xs-5">
-                <input type="number" class="form-control" name="newWeight" placeholder="<?php echo $oldWeight;?>" min="0">
+                <input type="number" class="form-control" name="newWeight" value="<?php echo $oldWeight;?>" min="0">
             </div>
         </div>
 
         <div class="form-group">
             <label class="control-label col-xs-2">Height (in)</label>
             <div class="col-xs-5">
-                <input type="number" class="form-control" name="newHeight" placeholder="<?php echo $oldHeight;?>" min="0">
+                <input type="number" class="form-control" name="newHeight" value="<?php echo $oldHeight;?>" min="0">
             </div>
         </div>
 
         <div class="form-group">
             <label class="control-label col-xs-2">Draft Year</label>
             <div class="col-xs-5">
-                <input type="number" class="form-control" name="newDraftYear" placeholder="<?php echo $oldDraftYear;?>" min="1946" max="2014">
+                <input type="number" class="form-control" name="newDraftYear" value="<?php echo $oldDraftYear;?>" min="1946" max="2014">
             </div>
         </div>
 
