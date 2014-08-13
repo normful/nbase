7608f743e64ed0632c0dc3a7bbf8d27de88851a0
Ubuntu <ubuntu@ip-172-31-31-81.us-west-2.compute.internal>
2014-08-13 18:33:00 +0000

    revert to using lowercase table names

--- project-final/insert_test-data.sql
+++ project-final/insert_test-data.sql
@@ -1,27 +1,27 @@
 LOAD DATA INFILE '/home/ubuntu/nbase/data/Division.txt' 
-    INTO TABLE NBA.Division
+    INTO TABLE nba.division
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/Venue.txt'
-    INTO TABLE NBA.Venue
+    INTO TABLE nba.venue
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/NBATeam_BelongsTo.txt'
-    INTO TABLE NBA.NBATeam_BelongsTo
+    INTO TABLE nba.nbateam_belongsto
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/NBAPlayer_PlaysFor.txt'
-    INTO TABLE NBA.NBAPlayer_PlaysFor
+    INTO TABLE nba.nbaplayer_playsfor
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/NBAStaff_WorksFor.txt'
-    INTO TABLE NBA.NBAStaff_WorksFor
+    INTO TABLE nba.nbastaff_worksfor
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/Sponsor_Endorses.txt'
-INTO TABLE NBA.Sponsor_Endorses
+    INTO TABLE nba.sponsor_endorses
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/NBAGame_Plays_PlayedAt.txt'
-    INTO TABLE NBA.NBAGame_Plays_PlayedAt
+    INTO TABLE nba.nbagame_plays_playedat
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/NBAReferee.txt'
-    INTO TABLE NBA.NBAReferee
+    INTO TABLE nba.nbareferee
     IGNORE 1 LINES;
 LOAD DATA INFILE '/home/ubuntu/nbase/data/Referees.txt'
-    INTO TABLE NBA.Referees
+    INTO TABLE nba.referees
     IGNORE 1 LINES;
